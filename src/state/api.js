009d6facfbc769c50a597082c7e48a29b56e0b82
ShaiShillo <shaishillo@gmail.com>
2024-08-17 14:05:49 +0300

    Initialize Redux Toolkit setup

---
+++ src/state/api.js
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  reducerPath: "adminApi",
+  tagTypes: ["User"],
+  endpoints: (build) => ({
+    getUsers: build.query({
+      query: (id) => `general/user/${id}`,
+      provideTags: ["User"]
+    })
+
+  })
+});
+
+export const {
+  useGetUsersQuery,
+} = api;
